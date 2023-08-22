40ad5b25c320072df5d502eb35f28e6dd08b1194
ILhankhondaker <ilhankhondaker@gmail.com>
2023-08-22 22:00:11 +0600

    pagesComponent

---
+++ src/Pages/Xyz.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Xyz = () => {
+    return (
+        <div>
+            <h1 className='text-center'>Ok lets start</h1>
+        </div>
+    );
+};
+
+export default Xyz;
